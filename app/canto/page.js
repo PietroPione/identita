48210df9b649e58866bc539b5fa828a27fff42bd
PietroPione <pietrofrlavoro@gmail.com>
2026-07-19 12:59:41 +0200

    feat: aggiorna video canto/trailer e limita branding YouTube
    Nuovi ID video per le pagine /canto e /trailer, con modestbranding, rel=0 e iv_load_policy=3 per ridurre il branding del canale nell'embed.

--- app/canto/page.js
+++ app/canto/page.js
@@ -7,7 +7,7 @@ export default function CantoPage() {
     <main className="fixed inset-0 bg-black">
       <iframe
         className="w-full h-full border-0"
-        src="https://www.youtube.com/embed/r5zXq_fORxE?autoplay=1"
+        src="https://www.youtube.com/embed/E5Vl6eeYmsw?autoplay=1&modestbranding=1&rel=0&iv_load_policy=3"
         title="Canto"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
--- app/trailer/page.js
+++ app/trailer/page.js
@@ -7,7 +7,7 @@ export default function TrailerPage() {
     <main className="fixed inset-0 bg-black">
       <iframe
         className="w-full h-full border-0"
-        src="https://www.youtube.com/embed/ptDep1TaK9o?autoplay=1"
+        src="https://www.youtube.com/embed/GUKAMha193w?autoplay=1&modestbranding=1&rel=0&iv_load_policy=3"
         title="Trailer"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
